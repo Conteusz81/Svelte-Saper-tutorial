299828fe1f92fd1f4ff51fd35a14a92c3cfadf0c
Piotr Pawlica <piotr.pawlica@codeconcept.pl>
2020-06-26 07:33:25 +0200

    got prefetch / date format changed

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -17,7 +17,7 @@
 
 <nav class="navbar" role="navigation" aria-label="main navigation">
 	<div class="navbar-brand">
-		<a href="/" class="navbar-item">
+		<a rel="prefetch" href="/" class="navbar-item">
 			<img src="favicon.png" alt="virus">
 		</a>
 		<span
@@ -35,8 +35,8 @@
 	</div>
 	<div class="navbar-menu" class:is-active={isOpen}>
 		<div class="navbar-start">
-			<a href="/" class="navbar-item"  class:is-active={segment === undefined}>Home</a>
-			<a href="/about" class="navbar-item"  class:is-active={segment === 'about'}>About</a>
+			<a rel="prefetch" href="/" class="navbar-item"  class:is-active={segment === undefined}>Home</a>
+			<a rel="prefetch" href="/about" class="navbar-item"  class:is-active={segment === 'about'}>About</a>
 		</div>
 	</div>
 </nav>
--- src/data/parsers.js
+++ src/data/parsers.js
@@ -82,7 +82,7 @@ function parseStats(rawStats) {
         hospitalized: format.number(rawStats.hospitalized),
         icu: format.number(rawStats.inIcuCurrently),
         tested: format.number(rawStats.totalTestResults),
-        updated: moment(rawStats.lastModified).format('LLLL')
+        updated: moment(rawStats.lastModified).format('lll')
     }
 }
 
